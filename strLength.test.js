51a16cfc24b90f98a26ca503f3916718aeab6e4e
Billions Joel <joelatugonza@gmail.com>
2022-01-17 12:08:50 +0300

    Add test spaced sentence

--- strLength.test.js
+++ strLength.test.js
@@ -4,3 +4,10 @@ test('is a number', () => {
 	expect(strlen('billions')).toBe(8);
 });
 
+test('is a number', () => {
+	expect(strlen('joel')).toBe(4);
+});
+
+test('is a number', () => {
+	expect(strlen('Billions joel')).toBe(12);
+});
